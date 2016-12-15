637067d2f65490474e26c4a4a65d4bed32c71b2a
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-12-15 13:37:57 +0100

    functional test, input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -13,8 +15,36 @@ class NewVisitorTest(unittest.TestCase):
         # Janek wchodzi na stronę nowej aplikacji to-do
         self.browser.get('http://localhost:8000')
 
-        # dostrzega w tytule strony, że jest to aplikacja to-do
+        # dostrzega w tytule strony i nagłówku, że jest to aplikacja to-do
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # jest proszony o wprowadzenie nowego zadania do wykonania
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # wpisuje "Shopping"
+        inputbox.send_keys('Shoppping')
+
+        # naciska ENTER, strona się uaktualnia i wyświetla wprowadzone zadanie
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        # szukamy wielu elementów, możemy dostać pustą listę
+        # w przypadku metody bez "s" dostaniemy wyjątek
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Shopping' for row in rows)
+        )
+
+        # oprócz tego wyświetlane jest pole do wprowadzenia kolejnego zadania
+        # Janek wpisuje "Sprzątanie"
+
+        # latarnia, mamy pewność, że do tego miejsca wszystko idzie dobrze
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
