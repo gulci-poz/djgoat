1f42d869b5fbeeb128e7e020f26f06be2eef05b4
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-12-15 09:20:15 +0100

    first functional test refined a bit

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 
-# if Firefox ESR is not the default Firefox
+browser = webdriver.Firefox()
+
+# jeśli Firefox ESR nie jest domyślnym Firefoksem
 # from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 # browser = webdriver.Firefox(firefox_binary=FirefoxBinary(firefox_path='c:\\firefox\\path'))
 
-browser = webdriver.Firefox()
+# Janek wchodzi na stronę nowej aplikacji to-do
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# dostrzega w tytule strony, że jest to aplikacja to-do
+assert 'To-Do' in browser.title, "Browser title was " + browser.title
+
+browser.quit()
