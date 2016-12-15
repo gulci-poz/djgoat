8762eef3ade6b7d8e5341a60f45685df987b9d48
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-12-15 21:21:26 +0100

    without HttpRequest

--- lists/tests.py
+++ lists/tests.py
@@ -1,18 +1,20 @@
 from django.core.urlresolvers import resolve
-from django.http import HttpRequest
 from django.test import TestCase
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
+    # nie testujemy third parties
+    # taki test wykonuje Django Test Client
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf-8')
+        # nie testujemy stałych
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
