36b9ee3795da10373bd5469f3452d921e343f870
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-12-16 09:49:23 +0100

    functional test fails, new element not present

--- functional_tests.py
+++ functional_tests.py
@@ -42,7 +42,8 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Shopping' for row in rows),
-            'New to-do item did not appear in table'
+            'New to-do item did not appear in table -- its text was:\n%s'
+            % table.text
         )
 
         # oprócz tego wyświetlane jest pole do wprowadzenia kolejnego zadania
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
     if request.method == 'POST':
-        return HttpResponse(request,
-                            'home.html',
-                            {'new_item_text': request.POST['item_text']}
-                            )
+        return render(request,
+                      'home.html',
+                      {'new_item_text': request.POST.get('item_text', '')}
+                      )
     return render(request, 'home.html')
