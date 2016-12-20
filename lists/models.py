27f198b872a1f734b6f7a44ee72b11ccb10d2d71
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-12-20 12:00:23 +0100

    model updated with list

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
 
+# nie usuwamy migracji, które były już zaaplikowane do bazy danych
+# dobra praktyka - nie usuwamy migracji, które były już commitowane do gita
+
+
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
