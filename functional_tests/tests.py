2136d0c55ceb33d14d80d71d10f0200338eeb927
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-12-23 11:40:18 +0100

    staging server configuration, notes update

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,24 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Janek wchodzi na stronę nowej aplikacji to-do
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # dostrzega w tytule strony i nagłówku, że jest to aplikacja to-do
         self.assertIn('To-Do', self.browser.title)
@@ -52,7 +67,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.check_for_row_in_list_table('2: Sprzątanie')
 
     def test_can_start_a_list_for_one_user(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Ubieranie choinki')
@@ -66,7 +81,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.check_for_row_in_list_table('2: Porządki w szafie')
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Ubieranie choinki')
@@ -85,7 +100,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Wojtek odwiedza stronę
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # # sprawdzamy, czy Wojtek widzi pozycje listy Janka
 
@@ -116,7 +131,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Janek uruchamia stronę startową
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # dostrzega wyśrodkowany inputbox
@@ -140,4 +155,4 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=10
         )
 
-        self.fail('Finish the test!')
+        # self.fail('Finish the test!')
